0e382eadf0fdc41b1dec267ce6603448cad427cc
mohammad karimpour <mohammad.karimpour1383@gmail.com>
2022-02-20 22:11:49 +0330

    'learning'

---
+++ datatype.js
@@ -0,0 +1,36 @@
+// دیتا تایپ ها 
+
+let number = 12;
+let number2 = 12.56;
+
+
+let string = 'mohammad';
+let string2 = "karimpour";
+let Fstring = `mohammad ${string2}`; // => mohammad karimpour
+
+
+let boolean = true; // +
+let boolean2 = false; // -
+
+
+let infinity = Infinity; // بی نهایت و بزرگ تر از هر عددی
+
+
+let nan = NaN; // نتیجه  محاصبات اشتباه
+
+
+let Null = null; // تهی
+
+
+let Undefined = undefined;  // تعریف نشده
+
+
+let symbol = Symbol("mohammad"); // برای شناسه های منحصر به فرد
+
+
+let object = Object; //برای ساختارهای داده پیچیده تر
+
+//----------------------------------------------
+
+console.log(typeof(Null)); // نوع تایپ را برمیگرداند
+
