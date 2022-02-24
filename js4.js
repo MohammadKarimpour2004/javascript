82f13a934042b9d5265b55f83b9cdef7f4c796fb
mohammad karimpour <mohammad.karimpour1383@gmail.com>
2022-02-24 12:12:22 +0330

    javascript

---
+++ js4.js
@@ -0,0 +1,16 @@
+//عملگر های منطقی
+let num1 = 23
+let num2 = 56
+
+console.log(num1 + num2); //برای استرینگ ها عملیات به هم چسپاندن را انجام میدهد
+
+console.log(num1 - num2);
+
+console.log(num1 * num2);
+
+console.log(num1 / num2);
+
+console.log(num1 ** num2); //tavan
+
+console.log(num1 % num2); //baqimondeh
+
---
+++ js5.js
@@ -0,0 +1,12 @@
+// سترینگ ها
+
+let Name = "mohammad";
+let family = "karimpour";
+
+console.log(Name + family); // mohammad Karimpour
+ 
+console.log(Name + "\n" + family); // mohammad
+                                   //   karimpour
+                                   
+
+console.log("mohammad\"karimpour\""); // mohammad "karimpour"                                   
