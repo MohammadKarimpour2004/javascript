2f1a5a9117818469c613a7c1f9b7dc1d0ca72e1b
mohammad karimpour <mohammad.karimpour1383@gmail.com>
2022-02-25 21:58:45 +0330

    javascript

---
+++ js15.js
@@ -0,0 +1,12 @@
+//حلقه ها
+
+/*
+for (mabda;shart;code ezafi) {
+    code
+    
+}*/
+
+
+for(let num = 0; num < 10; num++){
+     console.log(num);
+}
---
+++ js16.js
@@ -0,0 +1,23 @@
+
+/*
+while (شرط) {
+    دستورات
+}*/
+
+let num = 0
+
+while (num < 100) {
+    console.log(num);
+    num++
+}
+
+//do while
+//حد اقل یک بار اجرا میشود که در دوو نوشته میشود
+
+do{
+    num++
+}
+while(num == -1 ){
+    console.log(num);
+    num++
+}
