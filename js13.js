0363c9815e7280f35796cd35a8f7cdd3d709991a
mohammad karimpour <mohammad.karimpour1383@gmail.com>
2022-02-25 16:44:53 +0330

    javascript

---
+++ js13.js
@@ -0,0 +1,19 @@
+// متد های Math
+
+console.log(Math.PI);//=> عدد پی را برمیگرداند
+
+console.log(Math.pow(2,13));//=>8192 عملیات توان را انجام میدهد
+
+console.log(Math.sqrt(16));//=> 4 جذر یک عدد را برمیگرداند
+
+console.log(Math.abs(-10));//=>10 مثپت یک عدد را برمیگرداند
+
+console.log(Math.min(12,23,34,45,56));//=> 12 کمترین مقدار را برمیگرداند
+
+console.log(Math.min(12,23,34,45,56));//=> 56 بیشتر مقدار را برمیگرداند
+
+console.log(Math.floor(3.28));//=> 3 اعشار را حذف میکند
+
+console.log(Math.round(34.8));//=>35 عدد را رند میکند
+
+console.log(Math.random());//=> عدد رندوم بین 0 و 1
