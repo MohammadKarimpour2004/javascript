995bcd6f1c0ac194bce217a1668cae3e85d890f3
mohammad karimpour <mohammad.karimpour1383@gmail.com>
2022-03-07 23:21:25 +0330

    javascript

--- js32.js
+++ js32.js
@@ -1,3 +1,8 @@
 let ul = document.createElement('ul')
 ul.innerHTML = `<li>mohammad</li>`
 ul.style.backgroundColor = 'red'
+
+// نحوه append  کردن المنت ساخته شده
+let div = document.querySelector('div')
+div.append(ul)
+div.appendChild(ul)
---
+++ js33.js
@@ -0,0 +1,28 @@
+let btn = document.querySelector('#btn')
+
+/* 
+باید در تگ اچ تی ام ال یک اتریبیوت بسازید و فانگشن مد نظر را بنویسید
+
+<button onClick='texts()'>text</button>
+
+<button onClick='onkeypress()'>text</button>
+
+<button onClick='onkeyup()'>text</button>
+
+<button onClick='onfocus()'>text</button>
+(onblur != onfocus)
+<button onClick='onblur()'>text</button>
+
+<button onClick='ondblclick()'>text</button>
+
+*/
+
+
+
+const texts = ()=>{
+    console.log('mohammad');
+}
+
+
+
+
