092407b1a7f2bc0f6fda51484fa2a9af2d4f8a5b
mohammad karimpour <mohammad.karimpour1383@gmail.com>
2022-03-09 13:06:52 +0330

    javascript

---
+++ js35.js
@@ -0,0 +1,10 @@
+let tag = document.querySelector('#app')
+
+//برای ادد کردن 
+tag.classList.add("green")
+
+//برای حذف کرد
+tag.classList.remove('green')
+
+//اگر وجود نداشت ادد میکند و اگر وجود داشت حذف میکند
+tag.classList.toggle('green')
