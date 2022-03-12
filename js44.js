e3654a70b6f8223242675f534fd4d33e7e3ff9da
mohammad karimpour <mohammad.karimpour1383@gmail.com>
2022-03-12 23:33:22 +0330

    javascript

---
+++ js44.js
@@ -0,0 +1,14 @@
+let time = new Date()
+
+time.getFullYear() // => سال
+
+time.getMonth() // => ایندکس ماه را برمیگرداند
+
+time.getDate() // => روز
+
+time.getHours() // => ساعت
+
+time.getMinutes() // => دقیقه
+
+time.getSeconds() // => ثانیه
+
