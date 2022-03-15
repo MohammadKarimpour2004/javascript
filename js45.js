780aa74ef23db0f7fce88ffbb989c43a70693f61
mohammad karimpour <mohammad.karimpour1383@gmail.com>
2022-03-15 12:45:05 +0330

    javascript

---
+++ js45.js
@@ -0,0 +1,9 @@
+//BOM
+//برای دسترسی به اطلاعات مرور گر کاربر
+
+//دسترسی به ابعاد صفحه مرورگر
+
+window.innerWidth//body size
+window.innerHeight
+window.outerWidth//full size braozer
+window.outerHeight
